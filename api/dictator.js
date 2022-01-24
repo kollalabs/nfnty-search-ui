184b7b3d2c0cc8ed1dc6cc530dcad6b4ab2b9261
Jason Newman <jason@getkolla.com>
2022-01-24 11:13:01 -0700

    ref correct var

--- api/dictator.js
+++ api/dictator.js
@@ -1,5 +1,5 @@
 
 module.exports = async (req, res) => { // this function will be launched when the API is called.
-  var d = process.env.DICTATOR_NAME
+  var d = process.env.DICTATORS_NAME
   res.status(200).json({ name: d });
 }
