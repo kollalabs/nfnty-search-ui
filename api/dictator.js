8f8fd6889bc5dda57b590a7f5c2ec1b3c7e238a6
Jason Newman <jason@getkolla.com>
2022-01-24 10:59:06 -0700

    add endpoint to get current dictators name

---
+++ api/dictator.js
@@ -0,0 +1,7 @@
+module.exports = async (req, res) => { // this function will be launched when the API is called.
+  try {
+    res.send(process.env.DICTATOR) // send the dictators name
+  } catch (err) {
+    res.send(err) // send the thrown error
+  }
+}
