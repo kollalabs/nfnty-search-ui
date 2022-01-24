79272408ed04ac25cd9e5bc82c9ed6da4e6c130e
Jason Newman <jason@getkolla.com>
2022-01-24 11:07:19 -0700

    update

--- api/dictator.js
+++ api/dictator.js
@@ -2,10 +2,6 @@
 const users = ["Bob Smith", "Guillaume Bibeau"];
 
 module.exports = async (req, res) => { // this function will be launched when the API is called.
-  try {
-    const { id } = req.query;
-    res.status(200).json({ user: users[Number(id)] });
-  } catch (err) {
-    res.send(err) // send the thrown error
-  }
+  const { id } = req.query;
+  res.status(200).json({ user: users[0] });
 }
