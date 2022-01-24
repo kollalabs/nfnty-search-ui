4f80ea02be50f85b2ccbf2f444eba971b10d35ca
Jason Newman <jason@getkolla.com>
2022-01-24 11:02:32 -0700

    update

--- api/dictator.js
+++ api/dictator.js
@@ -1,6 +1,9 @@
 module.exports = async (req, res) => { // this function will be launched when the API is called.
   try {
-    res.send(process.env.DICTATORS_NAME) // send the dictators name
+    data = {
+      "name": process.env.DICTATORS_NAME
+    }
+    res.send(data) // send the dictators name
   } catch (err) {
     res.send(err) // send the thrown error
   }
