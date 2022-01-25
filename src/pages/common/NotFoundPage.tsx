97c1b0dc927ade89185ea347235d207d312bf963
Tyson Phalp <tyson@getkolla.com>
2022-01-25 15:21:13 -0700

    Trying to fix vercel build

--- src/pages/common/NotFoundPage.tsx
+++ src/pages/common/NotFoundPage.tsx
@@ -1,17 +1,17 @@
 import React, { useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 
 const NotFoundPage = () => {
-  let location = useLocation();
+  const location = useLocation();
 
   useEffect(() => {
-    if (location.pathname === '/start.html') {
+    if (location.pathname === '/start.html' || location.pathname === '/') {
       window.location.href = `${window.location.origin}/start.html`;
     }
-
   });
 
-  return null;
+  return <Navigate to={'/login'} replace />;
 };
 
 export default NotFoundPage;
