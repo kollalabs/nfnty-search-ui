9ebac76202879a0f08805efb988b859fd848e399
Jason Newman <jason@getkolla.com>
2022-01-25 01:11:39 +0000

    add try catch

--- api/search-kolla.js
+++ api/search-kolla.js
@@ -15,20 +15,27 @@ const JWKS = jose.createRemoteJWKSet(
 
 module.exports = async (req, res) => {
   // this function will be launched when the API is called.
+  try {
+    jwt = req.headers['authentication']
+    jwt = jwt.replace('Bearer ', '')
 
-  jwt = req.headers['authentication']
-  jwt = jwt.replace('Bearer ', '')
+    // https://github.com/panva/jose/blob/main/docs/interfaces/jwt_verify.JWTVerifyOptions.md
+    options = {
+      algorithms: ['RS256'],
+      issuer: 'https://infinitysearch.us.auth0.com/',
+      audience: 'https://infinitysearch.xyz'
+    }
 
-  // https://github.com/panva/jose/blob/main/docs/interfaces/jwt_verify.JWTVerifyOptions.md
-  options = {
-    algorithms: ['RS256'],
-    issuer: 'https://infinitysearch.us.auth0.com/',
-    audience: 'https://infinitysearch.xyz'
+    // token, JWTVerifyGetKey, JWTVerifyOptions
+    const { payload, protectedHeader } = await jose.jwtVerify(
+      jwt,
+      JWKS,
+      options
+    )
+    // https://github.com/panva/jose/blob/main/docs/interfaces/types.JWTVerifyResult.md
+  } catch (error) {
+    return res.status(400).json({ error: 'My custom 400 error' })
   }
-
-  // token, JWTVerifyGetKey, JWTVerifyOptions
-  const { payload, protectedHeader } = await jose.jwtVerify(jwt, JWKS, options)
-  // https://github.com/panva/jose/blob/main/docs/interfaces/types.JWTVerifyResult.md
 
   subscriber = payload['sub']
   console.log(subscriber)
