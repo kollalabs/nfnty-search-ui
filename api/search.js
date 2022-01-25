054106f9e4f1d7ded808cf6447b332205285f217
Jason Newman <jason@getkolla.com>
2022-01-25 21:54:33 +0000

    promote to main route

--- api/search.js
+++ api/search.js
@@ -1,9 +1,60 @@
-module.exports = async (req, res) => {
-  // this function will be launched when the API is called.
-  res.status(200).json(result)
+import * as jose from 'jose'
+
+// setup auth0 remote keyset
+const JWKS = jose.createRemoteJWKSet(
+  new URL('https://infinitysearch.us.auth0.com/.well-known/jwks.json')
+)
+
+// https://github.com/panva/jose/blob/main/docs/interfaces/jwt_verify.JWTVerifyOptions.md
+const JWTOptions = {
+  algorithms: ['RS256'],
+  issuer: 'https://infinitysearch.us.auth0.com/'
+  // audience: 'https://infinitysearch.xyz'
 }
 
-const result = {
+// this function will be launched when the API is called.
+module.exports = async (req, res) => {
+  var subscriber
+
+  try {
+    subscriber = await validateRequest(req)
+  } catch (err) {
+    return res.status(401).json({ error: err.message })
+  }
+  // TODO: lookup jobnimbus token using subscriber
+  // TODO: make request to kolla for data
+  // TODO: return the results
+
+  sampleResult['subscriber'] = subscriber
+  // for now just return the subscriber
+  res.status(200).json(sampleResult)
+}
+
+// handler to validate the JWT authentication token
+async function validateRequest (req) {
+  const authHeader = req.headers['authorization']
+  const parts = authHeader.split(' ')
+
+  if (parts.length !== 2 || parts[0] !== 'Bearer') {
+    throw 'invalid authentication header'
+  }
+
+  const jwt = parts[1]
+
+  // token, JWTVerifyGetKey, JWTVerifyOptions
+  const { payload, protectedHeader } = await jose.jwtVerify(
+    jwt,
+    JWKS,
+    JWTOptions
+  )
+  // https://github.com/panva/jose/blob/main/docs/interfaces/types.JWTVerifyResult.md
+
+  const subscriber = payload['sub']
+
+  return subscriber
+}
+
+const sampleResult = {
   jobnimbus: {
     meta: {
       logo: 'https://api.jobnimbus.kolla.dev/assets/img/logo-main.png',
