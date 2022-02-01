4de3b692a4b2dacfb98971d92b5e0812d1a11dc2
Jason Newman <jason@getkolla.com>
2022-02-01 16:42:49 -0700

    getting closer

--- api/search.go
+++ api/search.go
@@ -3,10 +3,12 @@ package api
 import (
 	"context"
 	"fmt"
+	"io"
 	"net/http"
+	"net/url"
 )
 
-var handlers = map[string]func(context.Context, tokenInfo, string) ([]SearchResult, SearchMeta, error){
+var handlers = map[string]func(context.Context, tokenInfo, string) (*SearchResults, error){
 	"connectors/job-nimbus": jobNimbusSearch,
 }
 
@@ -25,7 +27,7 @@ func SearchHandler(w http.ResponseWriter, r *http.Request) {
 		if !ok {
 			continue
 		}
-		_, _, err := f(ctx, appToken, r.URL.Query().Get("filter"))
+		_, err := f(ctx, appToken, r.URL.Query().Get("filter"))
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -34,23 +36,62 @@ func SearchHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-type SearchResponse map[string]struct {
-	Meta    SearchMeta     `json:"meta"`
-	Results []SearchResult `json:"results"`
+type SearchResponse map[string]SearchResults
+
+type SearchResults struct {
+	Meta    ConnectorMeta     `json:"meta"`
+	Results []ConnectorResult `json:"results"`
 }
 
-type SearchMeta struct {
+type ConnectorMeta struct {
+	Name        string `json:"name"`
 	Logo        string `json:"logo"`
 	DisplayName string `json:"display_name"`
 }
 
-type SearchResult struct {
+type ConnectorResult struct {
 	Title       string            `json:"title"`
 	Description string            `json:"description"`
 	Link        string            `json:"link"`
 	Kvdata      map[string]string `json:"kvdata,omitempty"`
 }
 
-func jobNimbusSearch(ctx context.Context, t tokenInfo, filter string) ([]SearchResult, SearchMeta, error) {
-	return nil, SearchMeta{}, fmt.Errorf("not implemented")
+const jobNimbusMediatorURL = "https://c-job-nimbus-7dgilp22pa-uc.a.run.app"
+
+func jobNimbusSearch(ctx context.Context, t tokenInfo, filter string) (*SearchResults, error) {
+	v := url.Values{
+		"filter": []string{"filter"},
+	}
+
+	u := jobNimbusMediatorURL + "/v1/contacts?" + v.Encode()
+	req, err := http.NewRequest(http.MethodGet, u, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", "Bearer "+t.AccessToken)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 300 {
+		b, _ := io.ReadAll(resp.Body)
+		return nil, fmt.Errorf("invlid status code: %d [%s]", resp.StatusCode, string(b))
+	}
+
+	// TODO: reformat response into the Infinity Search format
+	result := SearchResults{
+		Meta:    jobNimbusMetadata,
+		Results: nil,
+	}
+
+	return &result, fmt.Errorf("not implemented")
+}
+
+var jobNimbusMetadata = ConnectorMeta{
+	Name:        "connectors/job-nimbus",
+	Logo:        "https://3401zs241c1u3z7ulj3z6g7u-wpengine.netdna-ssl.com/wp-content/uploads/2020/10/cropped-5.-JN_Logo_Social_Submark_Condensed-Blue-Copy-3@1x-32x32.png",
+	DisplayName: "JobNimbus",
 }
