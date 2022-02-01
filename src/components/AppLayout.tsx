cf6f4b2b9d1909fccd7c5fee2232139c4a426537
Tyson Phalp <tyson@getkolla.com>
2022-02-01 10:58:53 -0700

    Refactored api to use react-query

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,56 +1,61 @@
-import AuthCallback from './auth/AuthCallback';
-import Connections from '../pages/connections/Connections';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
-import Logout from '../pages/auth/Logout';
 import React from 'react';
-import { AppSearchProvider } from '../contexts/SearchContext';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Route, Routes } from 'react-router-dom';
 
+import AuthCallback from './auth/AuthCallback';
+import Connections from '../pages/connections/Connections';
 import Footer from './layout/footer/Footer';
 import FooterNavMenu from './layout/footer/FooterNavMenu';
 import Header from './layout/header/Header';
 import HeaderNavMenu from './layout/header/HeaderNavMenu';
 import Login from '../pages/auth/Login';
+import Logout from '../pages/auth/Logout';
 import NotFoundPage from '../pages/common/NotFoundPage';
 import RequireAuth from './auth/RequireAuth';
 import Search from '../pages/search/Search';
-import { Route, Routes } from 'react-router-dom';
+import { AppSearchProvider } from '../contexts/SearchContext';
+
+const queryClient = new QueryClient();
 
 const AppLayout = () => {
   return (
     <Container disableGutters>
-      <AppSearchProvider>
-        <Header>
-          <HeaderNavMenu />
-        </Header>
-        <Grid item xs={12} sx={{ p: 2 }}>
-          <Routes>
-            <Route path={'*'} element={<NotFoundPage />} />
-            <Route path={'/auth-callback'} element={<AuthCallback />} />
-            <Route path={'/login'} element={<Login />} />
-            <Route path={'/logout'} element={<Logout />} />
-            <Route
-              path={'/search'}
-              element={
-                <RequireAuth>
-                  <Search />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path={'/connectors'}
-              element={
-                <RequireAuth>
-                  <Connections />
-                </RequireAuth>
-              }
-            />
-          </Routes>
-        </Grid>
-        <Footer>
-          <FooterNavMenu />
-        </Footer>
-      </AppSearchProvider>
+      <QueryClientProvider client={queryClient}>
+        <AppSearchProvider>
+          <Header>
+            <HeaderNavMenu />
+          </Header>
+          <Grid item xs={12} sx={{ p: 2 }}>
+            <Routes>
+              <Route path={'*'} element={<NotFoundPage />} />
+              <Route path={'/auth-callback'} element={<AuthCallback />} />
+              <Route path={'/login'} element={<Login />} />
+              <Route path={'/logout'} element={<Logout />} />
+              <Route
+                path={'/search'}
+                element={
+                  <RequireAuth>
+                    <Search />
+                  </RequireAuth>
+                }
+              />
+              <Route
+                path={'/connectors'}
+                element={
+                  <RequireAuth>
+                    <Connections />
+                  </RequireAuth>
+                }
+              />
+            </Routes>
+          </Grid>
+          <Footer>
+            <FooterNavMenu />
+          </Footer>
+        </AppSearchProvider>
+      </QueryClientProvider>
     </Container>
   );
 };
--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -5,35 +5,50 @@ import React from 'react';
 import NoResults from '../../components/search/NoResults';
 import PreSearch from '../../components/search/PreSearch';
 import SearchBar from '../../components/search/SearchBar';
-import useApi from '../../hooks/Api';
 import useAuthCheck from '../../hooks/AuthCheck';
 import useDocumentTitle from '../../hooks/DocumentTitle';
+import useReactQuery from '../../hooks/ReactQuery';
+import { Connection } from '../../models/DataModels';
 import { SearchItems } from './SearchItems';
 import { useAppSearch } from '../../contexts/SearchContext';
 import { useDebounce } from '../../hooks/Utilities';
 
 const Search = () => {
-  let companies: string[] = [];
+  let results: string[] = [];
 
   useDocumentTitle('App Search');
   useAuthCheck();
   const { query } = useAppSearch();
   const debouncedValue = useDebounce<string>(query, 200);
-  const { loading, error, data, refresh } = useApi(`/api/search?text=`, debouncedValue, 'cl');
 
-  if (data) {
-    // companies...
-    companies = Object.keys(data);
+  const searchQuery = useReactQuery<Connection>(
+    ['search', debouncedValue],
+    `search?text=${debouncedValue}`,
+    {
+      method: 'GET',
+    },
+    {
+      enabled: Boolean(debouncedValue.length > 0),
+    }
+  );
+
+  if (searchQuery.data) {
+    // results...
+    results = Object.keys(searchQuery.data);
   }
 
   return (
     <>
       <SearchBar />
-      {loading && <CircularProgress />}
-      {error && <CheckError error={error} apiRefresh={refresh} />}
-      {!loading && !error && query.length === 0 && <PreSearch message={'Hey, start searching!'} />}
-      {companies.length > 0 && SearchItems(companies, data)}
-      {!loading && query.length > 0 && companies.length === 0 && <NoResults />}
+      {searchQuery.isFetching && <CircularProgress />}
+      {searchQuery.error instanceof Error && (
+        <CheckError error={searchQuery.error} apiRefresh={searchQuery.refetch} />
+      )}
+      {!searchQuery.isFetching && !searchQuery.error && query.length === 0 && (
+        <PreSearch message={'Hey, start searching!'} />
+      )}
+      {results.length > 0 && SearchItems(results, searchQuery.data)}
+      {!searchQuery.isFetching && query.length > 0 && results.length === 0 && <NoResults />}
     </>
   );
 };
