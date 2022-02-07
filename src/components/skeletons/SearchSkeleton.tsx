cd3f9410ff4f4aa742e1a3e7b1e1d48d3b29f0e6
Tyson Phalp <tyson@getkolla.com>
2022-02-07 10:55:39 -0700

    Small style changes

--- src/components/skeletons/SearchSkeleton.tsx
+++ src/components/skeletons/SearchSkeleton.tsx
@@ -11,13 +11,12 @@ function SearchSkeleton() {
           alignItems: 'center',
         }}
       >
-        <Skeleton animation="wave" variant="circular" width={70} height={70} sx={{ mr: 2 }} />
+        <Skeleton animation="wave" variant="circular" width={55} height={55} sx={{ mr: 2 }} />
         <Skeleton animation="wave" height={50} width="20%" style={{ marginBottom: 0 }} />
       </Box>
-      <Skeleton animation="wave" height={50} width="80%" style={{ marginBottom: 0 }} />
-      <Skeleton animation="wave" height={50} width="75%" style={{ marginBottom: 0 }} />
-      <Skeleton animation="wave" height={50} width="70%" style={{ marginBottom: 0 }} />
-      <Skeleton animation="wave" height={50} width="77%" style={{ marginBottom: 8 }} />
+      <Skeleton animation="wave" height={50} width="100%" style={{ marginBottom: 0 }} />
+      <Skeleton animation="wave" height={50} width="95%" style={{ marginBottom: 0 }} />
+      <Skeleton animation="wave" height={50} width="100%" style={{ marginBottom: 0 }} />
     </>
   );
 }
