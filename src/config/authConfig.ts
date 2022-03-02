cd7c22007476ad7ad0a2360762bf969d2eca3e21
Jason Newman <jason@getkolla.com>
2022-03-02 22:48:11 +0000

    try not to use custom domain

--- src/config/authConfig.ts
+++ src/config/authConfig.ts
@@ -1,9 +1,9 @@
 const authConfig: any = {
   audience: 'https://infinitysearch.xyz',
   clientId: '2feXxLBCFHqtoNA05PdcrI3aqVsXbvu4',
-  domain: 'auth.infinitysearch.xyz',
+  // domain: 'auth.infinitysearch.xyz',
   scope: 'openid profile email',
-
+  domain: 'infinitysearch.us.auth0.com',
   useRefreshTokens: true,
   cacheLocation: 'localstorage'
 };
